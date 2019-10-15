a6ca6e5d395b2508ed73535ddaae9a3da4b77394
abe yukari <sakura@AbenoMacBook-Air.local>
2019-10-15 20:57:13 +0900

    deviseのコントローラーを消したところ、scss が当たらなくなった
    [![Image from Gyazo](https://i.gyazo.com/b06a42317606854cb5988da40a0209d9.png)](https://gyazo.com/b06a42317606854cb5988da40a0209d9)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'top#index'
-  devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions' , 
-  :passwords => "users/passwords"
-} 
-
-devise_scope :user do
-  # get "/signup", :to => "users/registrations#new"
-  get "signup" , to: "users/registrations#new"
-end
 end
