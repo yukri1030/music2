b09b1dd48b204fc9a18c0fa76ce3c01dce13e5a7
abe yukari <sakura@AbenoMacBook-Air.local>
2019-10-27 14:20:42 +0900

    削除ボタンを実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -14,9 +14,17 @@ class TopController < ApplicationController
     redirect_to root_path,notice: 'コメントを作成しました'
   end
 
+  def destroy
+    # binding.pry
+    comment = Top.find(params[:id])
+    if comment.user_id == current_user.id
+      comment.destroy
+    end
+  end
+
   private
   def top_params
-    params.permit(:name, :image, :text)
+    params.permit(:name, :image, :text, :id)
   end
 
   def move_to_index
